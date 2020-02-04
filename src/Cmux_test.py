0ae9b4a154ba318cb580abbdae9d0716be5d00c2
yanjunjie <yanjunjie@airm2m.com>
2020-02-04 08:46:12 -0800

    init linux cmux  operation

--- src/Cmux_test.py
+++ src/Cmux_test.py
@@ -4,5 +4,8 @@ import os
 class Cmux_test:
     def __init__(self, port):
         self.port = port
-        res = os.popen('./bin/cmux /dev/ttyUSB0').read()
+        res = os.popen('./bin/cmux %s' % port).read()
         print(res)
+
+
+Cmux_test('/dev/ttyUSB0')
