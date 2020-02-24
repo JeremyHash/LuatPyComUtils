7f8d614d6307ecdf5919792a0ef4fc7b7e096bbb
yanjunjie <yanjunjie@airm2m.com>
2020-02-25 03:26:45 +0800

    init auto

--- src/utils/Auto.py
+++ src/utils/Auto.py
@@ -6,3 +6,9 @@ class Auto:
     def get_com(self, com_name):
         port = os.popen(f".././bin/dev_name {com_name}").read()
         return port
+
+
+if __name__ == '__main__':
+    com_name = input('请输入要识别的端口：')
+    port = Auto().get_com(com_name)
+    print(f'端口号为：{port}')
