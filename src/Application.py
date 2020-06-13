c416334e5332ba1c23f7ad06c6c7a9022b66aafb
hashjeremy <hashjeremy@163.com>
2020-06-13 19:11:03 +0800

    update Application.py

--- src/Application.py
+++ src/Application.py
@@ -11,76 +11,65 @@ import signal
 # Application类
 class Application:
 
+    # 构造方法
     def __init__(self, port, baud_rate, ATListFileNames, loopTimes):
         self.port = port
         self.baud_rate = baud_rate
         self.ATListFileNames = ATListFileNames
         self.loopTimes = loopTimes
 
+    # 程序运行方法
     def run(self):
         app = ATestUtils.ATestUtils(self.port, self.baud_rate)
         app.ATest(self.ATListFileNames, self.loopTimes)
         print(f'共发生错误{app.error_count}次')
 
 
-# 定义ATListFiles
+# 定义ATListFiles，用来储存要运行的ATList文件列表
 ATListFileNames = []
-# 初始化波特率设置
+# 初始化串口波特率设置
 baud_rate = 115200
 # trace控制选项，n为关闭，y为打开
 enable_trace = 'n'
-# 诊断端口
+# 诊断进程pid
 diag_pid = 0
+# 查询系统平台
+system_cate = platform.system()
+print(f'当前操作系统为：{system_cate}')
 
 try:
+    # 如果控制台传入的参数数量为2且第二个参数为-h,则显示运行帮助
     if len(sys.argv) == 2 and sys.argv[1] == '-h':
         print('''   useage: 
             Linux: nohup python3 Application {port} {loopTimes}&!
             Windows: python Application {port} {loopTimes}''')
         sys.exit(0)
 
-    if len(sys.argv) == 1:
+    # 如果控制台传入的参数数量为1，则说明没有用户自定义参数，此时询问用户输入必要信息，串口，循环次数，测试功能
+    elif len(sys.argv) == 1:
+        # 请求输入循环次数
         loopTimes = int(input('请输入循环次数：'))
-        # 查询系统平台
-        system_cate = platform.system()
-        print(f'当前操作系统为：{system_cate}')
+
         # 显示当前所有端口（在Linux下使用python命令要指明python3）
         if system_cate == 'Linux':
             ports = os.popen('python3 -m serial.tools.list_ports').read()
             print(ports)
+        # macOS系统平台为Darwin
         elif system_cate == 'Darwin':
             ports = os.popen('python3 -m serial.tools.list_ports').read()
             print(ports)
         else:
             ports = os.popen('python -m serial.tools.list_ports').read()
             print(ports)
+        # 如果没有查询到端口，则提示用户需要连接模块
         if "" == ports:
             print("没有检测到端口，请连接模块")
             sys.exit(0)
+        # 请求输入端口号
         port = input('请输入测试设备端口号：')
-    elif len(sys.argv) == 3:
-        port = sys.argv[1]
-        loopTimes = int(sys.argv[2])
-    else:
-        print('-------------------------------------')
-        print('参数有误,使用方法请添加帮助参数:-h')
-        print('-------------------------------------')
-        sys.exit(0)
-    while True:
-        if len(sys.argv) == 3:
-            ATListFileNames.append('INIT.txt')
-            ATListFileNames.append('BASE.txt')
-            ATListFileNames.append('FILE.txt')
-            ATListFileNames.append('TCPIP.txt')
-            ATListFileNames.append('FTP.txt')
-            ATListFileNames.append('HTTP.txt')
-            ATListFileNames.append('MQTT.txt')
-            # ATListFileNames.append('SMS.txt')
-            ATListFileNames.append('PB.txt')
-            ATListFileNames.append('AUDIO.txt')
-            # ATListFileNames.append('TMP.txt')
-            break
-        elif len(sys.argv) == 1:
+
+        # 请求输入测试功能
+        while True:
             fileName = input('请输入要测试的功能（INIT,BASE,FILE,TCPIP,FTP,HTTP,MQTT,SMS,PB,AUDIO,TMP），输入END结束，全选请输入ALL：')
             if fileName == 'END':
                 break
@@ -102,11 +91,27 @@ try:
                 print('输入的功能名称有误,请重新输入')
                 continue
             ATListFileNames.append(f'{fileName}.txt')
-        else:
-            print('-------------------------------------')
-            print('参数有误,使用方法请添加帮助参数:-h')
-            print('-------------------------------------')
-            sys.exit(0)
+
+    # 如果控制台传入的参数数量为3，格式应为python Application.py /dev/ttyUSB0 1 将相应的参数传给相应变量,并填充ATListFileNames列表
+    elif len(sys.argv) == 3:
+        port = sys.argv[1]
+        loopTimes = int(sys.argv[2])
+        ATListFileNames.append('INIT.txt')
+        ATListFileNames.append('BASE.txt')
+        ATListFileNames.append('FILE.txt')
+        ATListFileNames.append('TCPIP.txt')
+        ATListFileNames.append('FTP.txt')
+        ATListFileNames.append('HTTP.txt')
+        ATListFileNames.append('MQTT.txt')
+        # ATListFileNames.append('SMS.txt')
+        ATListFileNames.append('PB.txt')
+        ATListFileNames.append('AUDIO.txt')
+        # ATListFileNames.append('TMP.txt')
+    else:
+        print('-------------------------------------')
+        print('参数有误,使用方法请添加帮助参数:-h')
+        print('-------------------------------------')
+        sys.exit(0)
 
     # 当前如果是Linux的话，询问用户是否开启trace抓取功能
     if system_cate == 'Linux':
